fix backprop issue, freeze all layers except for embedding

COMET/comet/models/estimators/estimator_base.py
@@ -199,11 +199,18 @@ class Estimator(ModelBase):
             cuda=False,
             batch_size = -1
     ):
+        self.eval()
+
+        # print('| num. model params: {} (num. trained: {})'.format(
+        #     sum(p.numel() for p in self.parameters()),
+        #     sum(p.numel() for p in self.parameters() if p.requires_grad),
+        # ))
+
         if cuda and torch.cuda.is_available():
             self.to("cuda")
 
         batch_size = self.hparams.batch_size if batch_size < 1 else batch_size
-        src_tokens = samples['src'] #bz X src_len
+        src_tokens = samples['src'] # bz X src_len
         src_length = torch.zeros(batch_size)
         src_length[:] = src_tokens.size(1)
         src_sentemb = self.get_sentence_embedding(src_tokens, src_length)
@@ -220,7 +227,7 @@ class Estimator(ModelBase):
         # case1: mt is the source's embedding -> adversarial examples generated
         if hy_len > 2:
             # hypo -> bz X trg_len X embd_dim
-            #TODO: should I use avg pooling ofencoder output to represent sent-embedding?
+            #TODO: should I use avg pooling of encoder output to represent sent-embedding?
             mt_sentemb = hypothesis[:, 0, :]
         # case2: mt is softmax output (for prediction)
         else:
@@ -231,11 +238,11 @@ class Estimator(ModelBase):
 
         prod_ref = mt_sentemb * ref_sentemb
         prod_src = mt_sentemb * src_sentemb
-        with torch.no_grad():
-            embedded_sequences = torch.cat(
-                (mt_sentemb, ref_sentemb, prod_ref, diff_ref, prod_src, diff_src), dim=1
-            )
-            score = self.ff(embedded_sequences)
+
+        embedded_sequences = torch.cat(
+            (mt_sentemb, ref_sentemb, prod_ref, diff_ref, prod_src, diff_src), dim=1
+        )
+        score = self.ff(embedded_sequences)
         return score
 
     def predict(

fairseq-0.9.0/fairseq/criterions/comet_score.py
@@ -32,6 +32,8 @@ class CometCriterion(FairseqCriterion):
         3) logging outputs to display while training
         """
 
+        # freeze update for all part of models except for embedding layer
+
         print("src shape: ", sample['net_input']['src_tokens'].shape)
         print("target shape: ", sample['target'].shape)
         net_output, _ = model(**sample['net_input']) #bz X tgt-len X output_dimension (vocab size)
@@ -42,7 +44,7 @@ class CometCriterion(FairseqCriterion):
         index_convert[0] = 0
         index_convert = torch.cumsum(index_convert, 0)
         softmax_output = softmax_output @ index_convert
-        softmax_output = softmax_output.long()
+        softmax_output = softmax_output.long() # convert softmax prediction into token format
         src_tokens = sample['net_input']['src_tokens']
 
         # wrap up data for prediction