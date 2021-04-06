correct the eos label, change BLEU score computation

fairseq-0.9.0/fairseq/criterions/mrt_bleu.py
@@ -53,12 +53,17 @@ class MRTBLEU(FairseqCriterion):
         #     0.0
         # ).float()
         num_subsample = 0
+        eos_indice = src_tokens.new_full(
+            (self.k, bsz),
+            0
+        )
         while num_subsample < self.k - 1:
             tokens = src_tokens.new_full((bsz, max_len + 2), pad)
             tokens[:, 0] = eos
-            sentence_prob = src_tokens.new_full((bsz, 1), 0.0)
+            # sentence_prob = src_tokens.new_full((bsz, 1), 0.0)
             # used for finished sentence
             is_decoding = src_tokens.new_ones(bsz).bool()
+            end_indice = src_tokens.new_zeros(bsz)
             for step in range(max_len + 1):
                 if is_decoding.sum() == 0:
                     break
@@ -68,17 +73,17 @@ class MRTBLEU(FairseqCriterion):
                 # apply softmax because probability needs to be tracked
                 lprob = torch.softmax(lprob, dim=1)
                 new_token = torch.multinomial(lprob, 1).squeeze() # bz x 1, need to squeeze later
-                # retrieve the probability
-                # prob = torch.gather(lprob, 1, new_token.clone())
-                # pad the already finished sentence and fix the prob
+                # new_token = torch.argmax(lprob, 1)
+
                 new_token = new_token.masked_fill_(
                     ~is_decoding,
                     pad
                 )
-                # Update is_decoding flag.
-                is_decoding = is_decoding * torch.ne(new_token, eos)
 
                 # prob[~is_decoding] = 1
+                # Update is_decoding flag.
+                is_decoding = is_decoding * torch.ne(new_token, eos)
+                end_indice += step * (~torch.ne(new_token, eos))
 
                 # sentence_prob = sentence_prob + prob.log()
                 tokens[:, step + 1] = new_token
@@ -91,21 +96,28 @@ class MRTBLEU(FairseqCriterion):
                     break
             if not hasDuplicate:
                 # add #bsz inferenced sample into output, along with their prob
+
+                # TODO: maybe this for-loop to modify eos can be eliminated somehow?
+                # eos is replaced by padding
+                for bz in range(bsz):
+                    tokens[bz, end_indice[bz]+1] = pad
                 out_indice[num_subsample, :, :] = tokens
+                eos_indice[num_subsample, :] = end_indice
                 # out_prob[num_subsample, :] = sentence_prob.squeeze(-1)
                 num_subsample += 1
 
         # add in the gold translation
-        pad_len = out_indice.size(2) - sample['net_input']['prev_output_tokens'].size(1) - 1
+        pad_len = out_indice.size(2) - sample['net_input']['prev_output_tokens'].size(1)
         end_of_sent = out_indice.new_full((bsz, 1), eos)
         padding = out_indice.new_full((bsz, pad_len), pad)
-        out_indice[self.k-1, :, :] = torch.cat((sample['net_input']['prev_output_tokens'], end_of_sent, padding), dim=1)
+        out_indice[self.k-1, :, :] = torch.cat((sample['net_input']['prev_output_tokens'], padding), dim=1)
+        eos_indice[self.k-1, :] = src_tokens.new_zeros(bsz)
         # k x bz x len => bz x k x len
         out_indice = out_indice.permute(1, 0, 2)
         # k x bz => bz x k
         # out_prob = out_prob.permute(1, 0)
-
-        return out_indice
+        eos_indice = eos_indice.permute(1, 0) # bz X k
+        return out_indice, eos_indice
 
     def forward(self, model, sample, reduce=True):
         """Compute the loss for the given sample.
@@ -115,15 +127,20 @@ class MRTBLEU(FairseqCriterion):
         2) the sample size, which is used as the denominator for the gradient
         3) logging outputs to display while training
         """
+
         eos = self.task.target_dictionary.eos()
         pad = self.task.target_dictionary.pad()
+
         # generate a sub sample space of size k
         with torch.no_grad():
-            indice = self.subsample(model, sample)
+            indice, eos_indice = self.subsample(model, sample)
+
+        model.train()
 
 
         ################################################################
         bz = indice.size(0)
+        tgt_size = indice.size(2)
         prev_output_token = indice.reshape(-1, indice.size(2))
         custom_input = sample
         custom_input['net_input']['src_tokens'] = sample['net_input']['src_tokens'].repeat_interleave(self.k, dim=0)
@@ -131,11 +148,23 @@ class MRTBLEU(FairseqCriterion):
         custom_input['net_input']['prev_output_tokens'] = prev_output_token
 
         # get log prob of batch X k samples
-
         net_output = model(**custom_input['net_input'])
+        # net_output = model(**sample['net_input'])
         lprobs = model.get_normalized_probs(net_output, log_probs=True)
+
         custom_target = prev_output_token.roll(-1, 1) # shift to left, along dim=1
         custom_target[:, -1] = pad
+        eos_indice = eos_indice.reshape(-1, 1)
+
+        for i in range(bz * self.k):
+            if (i+1) % self.k == 0:
+                batch_num = int((i+1)/self.k - 1)
+                # gold sentence case
+                pad_sent = custom_target.new_ones(tgt_size - sample['target'].size(1)) * pad
+                custom_target[i, :] = torch.cat((sample['target'][batch_num], pad_sent))
+            else:
+                custom_target[i, eos_indice[i]] = eos
+
         sent_prob = lprobs.gather(-1, custom_target.unsqueeze(-1)).squeeze()
         non_pad_mask = custom_target.ne(pad)
         sent_prob = sent_prob * non_pad_mask
@@ -157,19 +186,20 @@ class MRTBLEU(FairseqCriterion):
             tgt_sent = self.task.target_dictionary.string(tgt_token, "sentencepiece", escape_unk=True)
             # print(tgt_sent)
             for j in range(self.k):
+                scorer.reset()
                 sys_token = indice[batch, j]
                 sys_token = utils.strip_pad(sys_token, self.task.target_dictionary.pad()).int()
                 sys_sent = self.task.target_dictionary.string(sys_token, "sentencepiece", escape_unk=True)
-                # print(sys_sent)
                 scorer.add_string(tgt_sent, sys_sent)
                 bleu_score = scorer.score()
                 all_score[batch, j] = bleu_score
             # print()
 
         # compute risk and perform backprop
-        # print(all_score)
 
-        loss = -torch.sum(Q * all_score) # loss = - risk
+
+        loss = torch.sum((1-Q) * all_score) # loss = - risk
+
         #########################################################
 
         sample_size = sample['target'].size(0) if self.args.sentence_avg else sample['ntokens']